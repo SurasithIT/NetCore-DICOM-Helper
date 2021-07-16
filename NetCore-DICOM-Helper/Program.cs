f9f2177a84609577e1dccb723a8f6d5a52afe0c7
Surasith.k <Surasith.k@kabintech.co.th>
2021-07-16 17:10:35 +0700

    first commit

---
+++ NetCore-DICOM-Helper/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Dicom;
+using Dicom.Imaging;
+
+namespace NetCore_DICOM_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Start convert DICOM file to Jpg image");
+                ImageManager.SetImplementation(new WinFormsImageManager());
+
+                string input = @"/Users/surasith/Downloads/example";
+                string output = @"/Users/surasith/Downloads/example.jpg";
+
+                ConvertDcmToJpg(input, output);
+                if (File.Exists(output))
+                {
+                    Console.WriteLine("Convert success");
+                }
+                else
+                {
+                    throw new Exception("Convert fail");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex);
+            }
+            
+            
+           
+        }
+
+        static void ConvertDcmToJpg(string inputFilePath, string outputFilePath)
+        {
+            bool exitsting = File.Exists(inputFilePath);
+
+            if (exitsting)
+            {
+                using (Stream stream = new FileStream(inputFilePath, FileMode.Open))
+                {
+                    DicomFile dicom = DicomFile.Open(stream);
+                    DicomImage image = new DicomImage(dicom.Dataset);
+                    var bitmap = image.RenderImage().AsBitmap();
+                    bitmap.Save(outputFilePath);
+                }
+            }
+            else
+            {
+                throw new Exception("Original file not found");
+            }
+        }
+    }
+}
